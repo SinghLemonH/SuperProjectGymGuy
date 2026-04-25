ce8760c8ab18b9ccf423fac927b7e8242b43ae35
SinghLemonH <singanthaiza@gmail.com>
2026-04-25 16:14:59 +0700

    feat: add auth.controller, auth.routes and update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,25 @@
-import { Hono } from "hono";
+import express from "express";
 import { testconn } from "./database/supabase";
-//import authRoutes from "./routes/auth.routes";
+import authRoutes from "./routes/auth.routes";
 
-const app = new Hono();
+const app = express();
+
+// ─── Middleware ────────────────────────────────────────────────────
+app.use(express.json()); // parse JSON body
 
 // ─── Routes ────────────────────────────────────────────────────────
-//app.route("/api/v1", authRoutes);
+app.use("/api/v1", authRoutes);
 
 // ─── Start Server ──────────────────────────────────────────────────
+const PORT = process.env.PORT || 3000;
+
 (async () => {
     if (await testconn()) {
         console.log("Database connection successfully!!")
     }
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    });
 })();
 
 export default app;
