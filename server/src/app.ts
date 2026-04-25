10c3de426302ec6d3a3130af6b5ed73fe0ed07f4
SinghLemonH <singanthaiza@gmail.com>
2026-04-25 15:59:29 +0700

    example of call api in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,17 @@
+import { Hono } from "hono";
 import { testconn } from "./database/supabase";
+//import authRoutes from "./routes/auth.routes";
 
+const app = new Hono();
 
+// ─── Routes ────────────────────────────────────────────────────────
+//app.route("/api/v1", authRoutes);
+
+// ─── Start Server ──────────────────────────────────────────────────
 (async () => {
     if (await testconn()) {
         console.log("Database connection successfully!!")
     }
-})()
+})();
+
+export default app;
