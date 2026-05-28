d8eeba806a8e458b927a7425207f4baeefe90441
SinghLemonH <singanthaiza@gmail.com>
2026-05-28 20:56:44 +0700

    fiexed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,9 @@ import ProtectedRoute from './components/layout/ProtectedRoute'
 import AppLayout      from './components/layout/AppLayout'
 import Login          from './pages/Login'
 import Register       from './pages/Register'
+import Profile        from './pages/Profile'
+import Dashboard      from './pages/Dashboard'
+import Leaderboard    from './pages/LeaderBoard'
 
 const Soon = ({ name }: { name: string }) => (
   <div className="flex items-center justify-center h-screen text-gray-400">
@@ -22,12 +25,12 @@ export default function App() {
         {/* Protected */}
         <Route element={<ProtectedRoute />}>
           <Route element={<AppLayout />}>
-            <Route path="/dashboard"   element={<Soon name="Dashboard" />} />
-            <Route path="/profile"     element={<Soon name="Profile" />} />
+            <Route path="/dashboard"   element={<Dashboard />} />
+            <Route path="/profile"     element={<Profile />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
             <Route path="/exercises"   element={<Soon name="Exercises" />} />
             <Route path="/plans"       element={<Soon name="Plans" />} />
             <Route path="/sessions"    element={<Soon name="Sessions" />} />
-            <Route path="/leaderboard" element={<Soon name="Leaderboard" />} />
             <Route path="/reports"     element={<Soon name="Reports" />} />
           </Route>
         </Route>
